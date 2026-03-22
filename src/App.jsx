63dd0927534b61faacf91a6e6bc96fe86789c55b
Oussema Cherif <ousseemacherif@gmail.com>
2026-03-22 01:46:40 +0100

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import Education from "../../Education";
 import Contact from "./Contact";
 import Experience from "./Experience";
-import Experience1 from "./Experience1";
 import Hero from "./Hero";
 import Projects from "./Projects";
 import Skills from "./Skills";
